b837e491d8b3d1249676ae33309058a4b8b48d5a
shubug1015 <shubug@naver.com>
2022-03-29 19:50:51 +0900

    Signup page 진행중

--- libs/api.ts
+++ libs/api.ts
@@ -4,6 +4,8 @@ const api = axios.create({
   baseURL: 'http://127.0.0.1:8000/',
 });
 
+interface ISignup {}
+
 export const signUpApi = {
   // checkId: (data) => api.get('users/signup/check_id/', { params: data }),
 
@@ -13,5 +15,27 @@ export const signUpApi = {
   // checkCertification: (data) =>
   //   api.get('users/signup/sms_authentication/', { params: data }),
 
-  // signUp: (data) => api.post('users/signup/', data),
+  signup: ({
+    name,
+    nickname,
+    phoneNum,
+    username,
+    password,
+    marketing,
+  }: {
+    name: string;
+    nickname: string;
+    phoneNum: string;
+    username: string;
+    password: string;
+    marketing: boolean;
+  }) =>
+    api.post('users/signup/', {
+      name,
+      nickname,
+      phone_number: phoneNum,
+      username,
+      password,
+      ad_agree: marketing,
+    }),
 };
--- pages/signup.tsx
+++ pages/signup.tsx
@@ -10,7 +10,7 @@ const SignUp: NextPage = () => {
     name: '',
     nickname: '',
     phoneNum: '',
-    code: '',
+    username: '',
     password: '',
     passwordCheck: '',
     service: false,
@@ -18,7 +18,15 @@ const SignUp: NextPage = () => {
     ageOver: false,
     marketing: false,
   });
+  const [code, setCode] = useState({
+    loading: false,
+    number: '',
+    notSended: true,
+    checked: false,
+  });
+  const [signupLoading, setSignupLoading] = useState(false);
 
+  // Input 필드 입력
   const handleInput = (
     e: React.ChangeEvent<HTMLInputElement>,
     kind: string
@@ -27,9 +35,25 @@ const SignUp: NextPage = () => {
       target: { value },
     } = e;
 
-    setInfos((prev) => ({ ...prev, [kind]: value }));
+    if (kind === 'code') {
+      //  인증번호 필드 입력
+      setCode((prev) => ({ ...prev, number: value }));
+    } else {
+      // 나머지 필드 입력
+      if (kind === 'phoneNum') {
+        // 전화번호 필드 변경 시 인증번호 필드 초기화
+        setCode((prev) => ({
+          ...prev,
+          number: '',
+          notSended: true,
+          checked: false,
+        }));
+      }
+      setInfos((prev) => ({ ...prev, [kind]: value }));
+    }
   };
 
+  // Checkbox 필드 입력
   const handleCheckbox = (
     e: React.ChangeEvent<HTMLInputElement>,
     kind: string
@@ -45,13 +69,32 @@ const SignUp: NextPage = () => {
     }
   };
 
+  // 인증번호 코드 전송
   const sendCode = async () => {
-    const code = await signUpApi.getCode(infos.phoneNum);
-
-    console.log(code);
+    setCode((prev) => ({ ...prev, loading: true }));
+    if (infos.phoneNum.length > 0) {
+      try {
+        await signUpApi.getCode(infos.phoneNum);
+        setCode((prev) => ({ ...prev, notSended: false }));
+      } catch {
+        alert('Server Error');
+      } finally {
+        setCode((prev) => ({ ...prev, loading: false }));
+      }
+    }
   };
 
-  //   console.log(infos);
+  const handleSubmit = async () => {
+    setSignupLoading(true);
+
+    try {
+      await signUpApi.signup(infos);
+    } catch {
+      alert('Server Error');
+    } finally {
+      setSignupLoading(true);
+    }
+  };
 
   return (
     <>
@@ -85,21 +128,32 @@ const SignUp: NextPage = () => {
             value={infos.phoneNum}
             kind='phoneNum'
             handleInput={handleInput}
+            codeLoading={code.loading}
             sendCode={sendCode}
           />
 
           <Input
+            disabled={code.notSended}
             label='인증번호'
-            type='text'
+            type='tel'
             placeholder='인증번호'
-            value={infos.code}
+            value={code.number}
             kind='code'
             handleInput={handleInput}
           />
 
           <Input
-            label='비밀번호'
+            label='아이디'
             type='text'
+            placeholder='아이디'
+            value={infos.username}
+            kind='username'
+            handleInput={handleInput}
+          />
+
+          <Input
+            label='비밀번호'
+            type='password'
             placeholder='비밀번호'
             value={infos.password}
             kind='password'
@@ -108,7 +162,7 @@ const SignUp: NextPage = () => {
 
           <Input
             label='비밀번호 확인'
-            type='text'
+            type='password'
             placeholder='비밀번호 확인'
             value={infos.passwordCheck}
             kind='passwordCheck'
@@ -144,8 +198,30 @@ const SignUp: NextPage = () => {
           </Checkbox>
         </div>
 
-        <div className='mt-12 flex h-[3.688rem] w-full cursor-pointer items-center justify-center rounded bg-[#00e7ff] text-lg font-medium text-[#282e38] transition-all hover:opacity-90'>
-          회원가입 완료
+        <div
+          onClick={handleSubmit}
+          className='mt-12 flex h-[3.688rem] w-full cursor-pointer items-center justify-center rounded bg-[#00e7ff] text-lg font-medium text-[#282e38] transition-all hover:opacity-90'
+        >
+          {signupLoading ? (
+            <svg
+              role='status'
+              className='h-6 w-6 animate-spin fill-[#373c46] text-[#02cce2]'
+              viewBox='0 0 100 101'
+              fill='none'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <path
+                d='M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z'
+                fill='currentColor'
+              />
+              <path
+                d='M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z'
+                fill='currentFill'
+              />
+            </svg>
+          ) : (
+            '회원가입 완료'
+          )}
         </div>
       </div>
     </>
