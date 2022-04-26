acaa47594a819d00eb31515bd95619285324549b
shubug1015 <shubug@naver.com>
2022-04-26 16:13:27 +0900

    image auto height

--- components/lecture/detail/info.tsx
+++ components/lecture/detail/info.tsx
@@ -1,21 +1,31 @@
 import Layout from '@layouts/sectionLayout';
 import Image from 'next/image';
+import { useState } from 'react';
 
 interface IProps {
   data: any[];
 }
 
 export default function Info({ data }: IProps) {
+  const [imgHeight, setImgHeight] = useState(0);
   return (
     <Layout padding='pb-80'>
       {data.map((i) => (
-        <div key={i.id} className='relative h-[1600rem] w-full'>
+        <div
+          key={i.id}
+          className='relative w-full'
+          style={{ height: imgHeight }}
+        >
           <Image
             src={i.detail_image}
             alt='Lecture Detail Image'
             layout='fill'
             objectFit='contain'
-            quality={100}
+            onLoad={({ target }) => {
+              const { clientWidth, naturalWidth, naturalHeight } =
+                target as HTMLImageElement;
+              setImgHeight((naturalHeight / naturalWidth) * clientWidth);
+            }}
           />
         </div>
       ))}
