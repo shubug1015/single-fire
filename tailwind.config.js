67d2bf3f288323fb1ecdcccf15f649a560b42a92
shubug1015 <shubug@naver.com>
2022-06-15 02:23:32 +0900

    태블릿 반응형 적용

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,16 +6,16 @@ module.exports = {
   theme: {
     extend: {},
     screens: {
-      '2xl': { max: '1535px' },
+      '2xl': { max: '15350px' },
       // => @media (max-width: 1535px) { ... }
 
-      xl: { max: '1279px' },
+      xl: { max: '12790px' },
       // => @media (max-width: 1279px) { ... }
 
-      lg: { max: '1023px' },
+      lg: { max: '10230px' },
       // => @media (max-width: 1023px) { ... }
 
-      md: { max: '767px' },
+      md: { max: '1179px' },
       // => @media (max-width: 767px) { ... }
 
       sm: { max: '639px' },
